82c290770ea4c7d057e6afe85f26a5c42f2c5773
rajon <mostofarajon111@gmail.com>
2022-03-03 01:28:01 +0600

    dynamic api created

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const API_KEY = `b0eb4aff182657ee55b7cde0ffe39151`
 
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
